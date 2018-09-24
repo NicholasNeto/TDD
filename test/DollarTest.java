03ad034330f00105c9b1eb48907d31629eb989b6
NicolsNeto <npneto@thoughtworks.com>
2018-09-24 15:04:19 -0300

    evolucao de test, verificamos que nos mostra um valor nao esperado

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -7,24 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DollarTest {
 
-    @Test
-    public void testMultiplication(){
 
+
+
+    @Test
+    public void testMultiplication2(){
 
         //Given -> dado que
         Dollar five = new Dollar(5);
 
 
+
         // When  -> quando
-
-         five.times(2);
-
+        five.times(2);
 
         // Then ->
+        int expectedResultMultiplication10 = 10;
+        assertEquals(expectedResultMultiplication10 , five.amount);
 
-        int expectedResultMultiplication = 10;
-        assertEquals(expectedResultMultiplication , five.amount);
+
+
+
+        // When  -> quando
+        five.times(3);
+
+        // Then ->
+        int expectedResultMultiplication15 = 15;
+        assertEquals(expectedResultMultiplication15 , five.amount);
+
     }
-
 
 }
