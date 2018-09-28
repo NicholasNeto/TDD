c3ae6b5d98df578ba8728f7d42192ff135cbd1c2
NicolsNeto <npneto@thoughtworks.com>
2018-09-27 21:17:40 -0300

    criando class Franc

--- src/Franc.java
+++ src/Franc.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Franc {
+
+    private int amount;
+
+    public Franc(int amount){
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier){
+        return new Franc(amount * multiplier);
+    }
+
+
+    public boolean equals(Object obj){
+        Franc franc = (Franc) obj;
+        return amount == franc.amount;
+    }
 }
