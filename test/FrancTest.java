71e8d6759325f6e2623d296b085484d1578f8c01
NicolsNeto <npneto@thoughtworks.com>
2018-09-28 11:56:59 -0300

    incrementando teste para varificação se são da mesma classa Dollar & Franc

--- test/FrancTest.java
+++ test/FrancTest.java
@@ -18,5 +18,7 @@ public class FrancTest {
     public void testEquality () {
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+
+        assertFalse(new Dollar(5).equals(new Franc(5)));
     }
 }
