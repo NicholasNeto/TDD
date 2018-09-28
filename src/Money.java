65b8b2d72eff47a391b2421b08571c275d009054
NicolsNeto <npneto@thoughtworks.com>
2018-09-28 11:29:30 -0300

    adicionado metodo equals em money

--- src/Money.java
+++ src/Money.java
@@ -2,4 +2,11 @@
 public class Money {
 
     protected int amount;
+
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
