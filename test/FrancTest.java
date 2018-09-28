8425ee7dc058d2db4d3c38aba3da5e0b529b4bbe
NicolsNeto <npneto@thoughtworks.com>
2018-09-28 11:16:45 -0300

    adcionado test para verificação de igualdade de Franc

--- test/FrancTest.java
+++ test/FrancTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FrancTest {
 
@@ -10,5 +12,11 @@ public class FrancTest {
 
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality () {
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
