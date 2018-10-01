7a1a284f1ba19427a0f246e7ab015539b96a0780
NicolsNeto <npneto@thoughtworks.com>
2018-10-01 20:45:57 -0300

    refatornado teste de New Dollar usando a fabrica de dollar in money Money.dollar

--- test/FrancTest.java
+++ test/FrancTest.java
@@ -19,6 +19,6 @@ public class FrancTest {
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
 
-        assertFalse(new Dollar(5).equals(new Franc(5)));
+        assertFalse(Money.dollar(5).equals(new Franc(5)));
     }
 }
--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -13,4 +13,13 @@ public class MoneyTest {
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
+
+
+    @Test
+    public void testEquality () {
+        assertTrue( Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+
+        assertFalse(Money.dollar(5).equals(new Franc(5)));
+    }
 }
