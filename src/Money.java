2dc8a5b0de12e8614ce1d5aff4a1f186f35514a3
NicolsNeto <npneto@thoughtworks.com>
2018-10-01 20:24:49 -0300

    começando a apagar a referencia de Dollar

--- src/Money.java
+++ src/Money.java
@@ -1,18 +1,19 @@
 
-public  class Money {
+public abstract class Money {
 
     protected int amount;
 
     public static Dollar dollar(int amount) {
         return new Dollar(amount);
     }
-
-
+    
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multipler);
 
 }
