f5dfe04166b4c6e6ea3b049619ad1f54fc325988
NicolsNeto <npneto@thoughtworks.com>
2018-10-07 14:59:18 -0300

    create methodo currency para implementar moedas e eliminar as subclasses

--- src/Dollar.java
+++ src/Dollar.java
@@ -2,12 +2,20 @@
 
 public class Dollar extends Money{
 
+    private String currency;
+
     public Dollar (int amount) {
         this.amount = amount;
+        currency = "USD";
     }
 
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public String currency() {
+        return currency;
     }
 
 }
