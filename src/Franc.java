ddb7061b0da6c9b9b93ee6365343d5d513aeddda
NicolsNeto <npneto@thoughtworks.com>
2018-10-07 15:00:44 -0300

    create methodo currency  in franc  for implements diferents kind money and take subclase

--- src/Franc.java
+++ src/Franc.java
@@ -2,11 +2,19 @@
 
 public class Franc extends Money {
 
+    private String currency;
+
     public Franc(int amount){
         this.amount = amount;
+        currency = "CHF";
     }
 
     Money times(int multiplier){
         return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public String currency() {
+        return currency;
     }
 }
